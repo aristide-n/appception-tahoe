b066f7107faabb658cc90ccd9d9c9e87a1c3532f
Aristide Niyungeko <niyungeko@gmail.com>
2013-06-24 01:19:30 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppceptionTahoe::Application.config.secret_token = 'ee2118b42affca18573b1b3be53fc1620e31c82e66fcae5f1961bffb6313de45c6a2fc95d390a04d30bace4fadd86a071a5b3743360dce88cd9f83c279674402'
