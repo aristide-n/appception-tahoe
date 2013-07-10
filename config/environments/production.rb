01be30c530d4c28b7a650be8f535ec787f939414
Aristide Niyungeko <niyungeko@gmail.com>
2013-07-09 19:18:19 -0700

    touch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ AppceptionTahoe::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+
   # Exceptions in production
   config.middleware.use ExceptionNotifier,
                         :email_prefix => "[Exception] ",
