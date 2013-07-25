c3a911b42edb8580f15ea43b1a1eecd22442764a
Aristide Niyungeko <niyungeko@gmail.com>
2013-07-25 12:58:23 -0700

    add destroy callback to destroy environment's selftests

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -1,5 +1,12 @@
 class Environment < ActiveRecord::Base
   attr_accessible :device_name, :platform, :user_agent
   has_many :selftests
+  before_destroy :destroy_selftests
+
+  def destroy_selftests
+    self.selftests.each do |selftest|
+      selftest.destroy
+    end
+  end
 
 end
