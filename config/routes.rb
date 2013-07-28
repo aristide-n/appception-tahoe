fa9c559eca3bc1d74442fcf343aec4ee0aa25743
Aristide Niyungeko <niyungeko@gmail.com>
2013-07-28 12:29:21 -0700

    add timeline charts

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ AppceptionTahoe::Application.routes.draw do
   match '/selftests/charts/:name' => 'selftests#opChart', :as => :operation
   match '/environments/charts/:env' => 'environments#deviceChart', :as => :chart
   root :to => "home#index"
+  match '/timelines/devices/all/:device_name' => 'timelines#show_device', :as => :device
+  match '/timelines/devices/all' => 'timelines#index_devices'
+  match '/timelines/tests/all/:test_name' => 'timelines#show_test', :as => :test
+  match '/timelines/tests/all' => 'timelines#index_tests'
 
 end
